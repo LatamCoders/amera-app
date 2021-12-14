f3e4b93e86feb6cf7ebf4966473e0698fa27e9b3
Jose Burgos <jose.b1996@hotmail.com>
2021-12-14 15:33:25 -0500

    comentarios en api

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,9 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::group(['prefix' => 'v1', 'middleware' => 'onlyAjax'], function () {
+    /*
+     * Auth
+     */
     Route::group(['prefix' => 'auth'], function () {
         /*
          * Controller: SelfPay
