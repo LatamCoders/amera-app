9d8175f64ad9e798792ceee9abb29417612a5e9f
Jose Burgos <jose.b1996@hotmail.com>
2021-12-14 15:28:28 -0500

    experiencias

---
+++ database/migrations/2021_12_14_202113_create_experiences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->integer('amera_rate');
+            $table->text('comments');
+            $table->foreignId('driver_id')->nullable()->constrained('drivers');
+            $table->foreignId('selfpay_id')->nullable()->constrained('selfpay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
